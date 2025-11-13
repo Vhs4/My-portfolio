f9fbd7a9f5f160d92225ae1ee09a0e03dfd37dfc
Vhs4 <vhszera44444@gmail.com>
2025-11-13 15:22:23 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,6 +124,15 @@ export default function RootLayout({
         />
 
         <link rel="canonical" href="https://vhs4.dev" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "u5kswen59t");`,
+          }}
+        />
       </head>
 
       <body className="font-sans">
